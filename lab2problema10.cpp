2a0bb934179e43553f006d77c95bf9c183dbd8d8
Fl3XINS <fanaconaperafan@gmail.com>
2023-03-26 23:46:49 -0500

    ajuste tinkerkat problema 10

--- lab2problema10.cpp
+++ lab2problema10.cpp
@@ -16,10 +16,13 @@ int main(){
 	}cout<< " Su Numero Romano : "<< numrom << " Es Igual a "<<valor <<" En Numero Romano";
 	
 }
-/*int NumerosArabes[7] = { 1000,500,100,50,10,5,1 };
-char NumerosRomanos[7] = { 'M','D','C','L','X','V','I' };
+/*
+const int MAX_LENGTH = 15;
+const int NUM_ROMANOS = 7;
+
+int NumerosArabes[NUM_ROMANOS] = { 1000,500,100,50,10,5,1 };
+char NumerosRomanos[NUM_ROMANOS] = { 'M','D','C','L','X','V','I' };
 int valor = 0;
-bool flag = false;
 String numrom = "";
 
 void setup() {
@@ -31,26 +34,30 @@ void loop() {
   while(Serial.available() == 0);
   numrom = Serial.readStringUntil('\n');
   
+  valor = 0; // reiniciar valor
+  bool flag=false;
   for(int i = 0; numrom[i]!= '\0'; i++){// iteramos hasta valor nulo de la cadena
-    for(int ii = 0 ; NumerosRomanos[ii] != '\0'; ii++){// itera sobre valores romanos
+    bool encontrado = false;
+    for(int ii = 0 ; ii < NUM_ROMANOS; ii++){// itera sobre valores romanos
       if(numrom[i]==NumerosRomanos[ii]){//comprobamos si el cada caractes es valido
+        encontrado = true;
         valor += NumerosArabes[ii];
-        flag=true;
-      }// si el caracter no es valido ingresar cadena nuevamente , contador se reinicia en 0
+        break;
+      }
     }
-    if(flag==false){
+    if(encontrado == false){
       Serial.println("Numero No Valido Ingrsa Nuevamente:");
+      flag = true;
       break;
     }
   }
-  if(flag==true){
+  if(flag == false){
     Serial.print("Su Numero Romano: ");
     Serial.print(numrom);
     Serial.print(" Es Igual a ");
     Serial.print(valor);
     Serial.println(" En Numero Romano");
   }
-  valor = 0;
-  flag = false;
 }
+
 /*
