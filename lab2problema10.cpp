d5a3c62fff353e411c08eded43ebf02ebd428ad4
Fl3XINS <fanaconaperafan@gmail.com>
2023-03-26 22:58:10 -0500

    problemas 6 y 8 en tinkerkat

--- lab2problema10.cpp
+++ lab2problema10.cpp
@@ -16,3 +16,41 @@ int main(){
 	}cout<< " Su Numero Romano : "<< numrom << " Es Igual a "<<valor <<" En Numero Romano";
 	
 }
+/*int NumerosArabes[7] = { 1000,500,100,50,10,5,1 };
+char NumerosRomanos[7] = { 'M','D','C','L','X','V','I' };
+int valor = 0;
+bool flag = false;
+String numrom = "";
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Ingresa Numero En Romano");
+  while(Serial.available() == 0);
+  numrom = Serial.readStringUntil('\n');
+  
+  for(int i = 0; numrom[i]!= '\0'; i++){// iteramos hasta valor nulo de la cadena
+    for(int ii = 0 ; NumerosRomanos[ii] != '\0'; ii++){// itera sobre valores romanos
+      if(numrom[i]==NumerosRomanos[ii]){//comprobamos si el cada caractes es valido
+        valor += NumerosArabes[ii];
+        flag=true;
+      }// si el caracter no es valido ingresar cadena nuevamente , contador se reinicia en 0
+    }
+    if(flag==false){
+      Serial.println("Numero No Valido Ingrsa Nuevamente:");
+      break;
+    }
+  }
+  if(flag==true){
+    Serial.print("Su Numero Romano: ");
+    Serial.print(numrom);
+    Serial.print(" Es Igual a ");
+    Serial.print(valor);
+    Serial.println(" En Numero Romano");
+  }
+  valor = 0;
+  flag = false;
+}
+/*
--- lab2problema6.cpp
+++ lab2problema6.cpp
@@ -11,3 +11,26 @@ char palabra[15];
 	cout << endl << "\nAll Palabras Mayusculas: " << palabra << endl;
 	return 0;
 }
+/*
+char palabra[15];
+String palabraMayus = "";
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  palabraMayus = "";
+  Serial.print("Ingrese Palabra: ");
+  while (Serial.available() == 0);
+  palabraMayus = Serial.readStringUntil('\n');
+  
+  for (int i = 0; i < palabraMayus.length(); i++) {
+    if (palabraMayus[i] <= 122 and palabraMayus[i] >= 97) {
+      palabraMayus[i] -= 32;
+    }
+  }
+  Serial.print("\nAll Palabras Mayusculas: ");
+  Serial.println(palabraMayus);
+}
+/*
