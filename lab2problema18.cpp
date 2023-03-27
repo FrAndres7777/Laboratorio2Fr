a8bd2fcf159cc08ad0ca32cfc5dc65a39c2072c2
Fl3XINS <fanaconaperafan@gmail.com>
2023-03-26 22:03:53 -0500

    comentarios

--- lab2problema18.cpp
+++ lab2problema18.cpp
@@ -4,9 +4,9 @@ int main(){
     char list[10]={'0','1','2','3','4','5','6','7','8','9'};
     int list2[8]={362880,40320,5040,720,120,24,6,2};
     int numero;
-    cout << "Ingresa tu numero" << endl;cin >> numero;
-    for(int i = 0, digito; i < 8; i++){
-        digito = numero / list2[i];
+    cout << "Ingresa tu numero" << endl;cin >> numero;//en el siguiente ciclo apartid de las permutaciones de cada indice calculamos indice a indice 
+    for(int i = 0, digito; i < 8; i++){//cada indice tiene cierta permutacion dependiendo del factorial de la pocision
+        digito = numero / list2[i];//lo que hago es ir aproximando digito a digito hasta aproximar 8 pocisiones 
         numero = numero - ((list2[i])*digito);
         if(numero == 0){
             digito = digito - 1;
